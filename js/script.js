8f73b7000d1a44970ab06a52cc210f0c7a74df19
Daniel <kuchta.daniel1@gmail.com>
2024-03-05 19:35:04 +0100

    Created empty form check

--- js/script.js
+++ js/script.js
@@ -7,6 +7,36 @@ const sendBtn = document.querySelector(".send")
 const popup = document.querySelector(".popup")
 
 
+const showError = (input, msg) => {
+    const formBox = input.parentElement;
+    const errorMsg = formBox.querySelector('.error-text');
+
+    formBox.classList.add('error');
+    errorMsg.textContent = msg;
+}
+
+const clearError = input => {
+    const formBox = input.parentElement;
+    formBox.classList.remove('error');
+}
+
+const emptyCheck = input => {
+    input.forEach(el => {
+        if(el.value === '') {
+            showError(el, el.placeholder)
+        } else {
+            clearError(el);
+        }
+    })
+}
+
+
+
+sendBtn.addEventListener('click', e => {
+	e.preventDefault();
+
+	emptyCheck([username, password, password2, email])
+})
 
 clearBtn.addEventListener("click", e => {
 	e.preventDefault();
